f30506f582dce67fc10b32aabae5ec9d07326ad6
danielaibar1994 <61664677+danielaibar1994@users.noreply.github.com>
2023-04-17 01:10:16 +0200

    [TASK-2] Set Signals in Movies Section (#1) (#2)
    * feat: push data server data
    
    * feat: [TASK-2] Set Signals in Movies Section

--- src/app/pages/movies/services/movies.service.ts
+++ src/app/pages/movies/services/movies.service.ts
@@ -1,39 +1,34 @@
 import { Injectable } from '@angular/core';
 import { Movie } from 'src/app/api/models/movie.interface';
-import { MoviesState } from 'src/app/services/states/movies/movies-state.service';
+import { MoviesSignal } from 'src/app/pages/movies/signals/movies/movies-signal.service';
 
 @Injectable({ providedIn: 'root' })
 export class MoviesService {
-    movies$ = this.state.getMovies$();
-    selectedMovie$ = this.state.getSelectedMovie$();
-    actors$ = this.state.getActors$();
-    companies$ = this.state.getCompanies$();
-    newMovie$ = this.state.getNewMovie$();
 
-    constructor(private readonly state: MoviesState) { }
+    constructor(private readonly store: MoviesSignal) { }
 
     getData(): void {
-        this.state.getMovies();
+        this.store.getMovies();
     }
 
     getMovieDetail(id: string): void {
-        this.state.getMovieDetail(id);
+        this.store.getMovieDetail(id);
     }
 
     getActors(): void {
-        this.state.getActors();
+        this.store.getActors();
     }
 
     getCompanies(): void {
-        this.state.getCompanies();
+        this.store.getCompanies();
     }
 
     editMovie(id: number, movie: Movie): void {
-        this.state.editMovie(id, movie);
+        this.store.editMovie(id, movie);
     }
 
     saveMovie(movie: Movie): void {
-        this.state.saveMovie(movie);
+        this.store.saveMovie(movie);
     }
 
 }
